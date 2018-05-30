1e5ee361ee283bb964eef5873d894a091c9338b5
bktran8 <33641377+bktran8@users.noreply.github.com>
2018-05-29 19:21:38 -0700

    Update p3helper.c

--- p3helper.c
+++ p3helper.c
@@ -1,29 +1,10 @@
-//VERSION ON EDORAS
 /* Brenda Tran
 * Professor Carroll
 * Program 3 assignment
-        * CS570
-* SDSU
-        * Spring 2018
-*
-* This is the ONLY file you are allowed to change. (In fact, the other
-        * files should be symbolic links to
-        *   ~cs570/Three/p3main.c
-        *   ~cs570/Three/p3robot.c
-        *   ~cs570/Three/p3.h
-        *   ~cs570/Three/makefile
-        *   ~cs570/Three/CHK.h    )
-* The file ~cs570/Three/createlinks is a script that will create these for you.
 */
 #include "p3.h"
 
 #define COURSEID "570"
-
-/* You may put declarations/definitions here.
-   In particular, you will probably want access to information
-   about the job (for details see the assignment and the documentation
-   in p3robot.c):
-     */
 extern int nrRobots;
 extern int quota;
 extern int seed;
